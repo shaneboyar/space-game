a3c7faf7989f4516965ef49bc432afc91a2354f5
Shane Boyar <shane.boyar@gmail.com>
2017-05-11 16:14:53 -0400

    Add ship class and get that sucka movin

--- game.rb
+++ game.rb
@@ -1,19 +1,33 @@
 require 'gosu'
-
+require './ship.rb'
 
 class GameWindow < Gosu::Window
   def initialize(*args)
     super
-
+    @ship = Ship.new
     @background = Gosu::Image.new('images/background.png', {tileable: true})
   end
 
   def button_down(button)
-    close if button == Gosu:: KbEscape
+    close if button == Gosu::KB_ESCAPE
   end
 
   def draw
     @background.draw(0, 0, 0)
+    @ship.draw
+  end
+
+  def update
+    if Gosu.button_down? Gosu::KB_LEFT
+      @ship.turn_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT
+      @ship.turn_right
+    end
+    if Gosu.button_down? Gosu::KB_UP
+      @ship.accelerate
+    end
+    @ship.move
   end
 end
 
---
+++ ship.rb
@@ -0,0 +1,40 @@
+require 'gosu'
+
+class Ship
+  ACCELERATION = 9.81
+
+  def initialize
+    @sprite = Gosu::Image.new('images/ship.png').subimage(0,0,32,32)
+    @position = { x: 375, y: 667, angle: 0.0 }
+    @velocity = { x: 0, y: 0 }
+    @score = 0
+  end
+
+  def turn_left
+    @position[:angle] -= 4.5
+  end
+
+  def turn_right
+    @position[:angle] += 4.5
+  end
+
+  def accelerate
+    @velocity[:x] += Gosu.offset_x(@position[:angle], 0.5)
+    @velocity[:y] += Gosu.offset_y(@position[:angle], 0.5)
+  end
+
+  def move
+    @position[:x] += @velocity[:x]
+    @position[:y] += @velocity[:y]
+
+    @position[:x] %= 750
+    @position[:y] %= 1334
+
+    @velocity[:x] *= 0.95
+    @velocity[:y] *= 0.95
+  end
+
+  def draw
+    @sprite.draw_rot(@position[:x], @position[:y], 1, @position[:angle])
+  end
+end
